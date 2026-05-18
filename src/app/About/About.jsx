9c8b707504ea378545ebd76231325cd96bfcc8cf
divneettkaurr <kaurdivneet2519@gmail.com>
2026-05-18 18:07:14 +0530

    about page

---
+++ src/app/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../../components/About/Hero'
+
+
+
+const About = () => {
+  return (
+    <div>
+      <Hero/>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/About/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div>
+
+            <div>
+                <img className='w-full ' src="/kt.png" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
